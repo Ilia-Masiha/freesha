c16cd2fcd2e1f55ae9139e46b4633f0f094414b6
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-10-27 19:15:52 +0330

    Created main router and a middleware - Created main.ts as the main router file - Created response.ts - Used main router in app.ts

--- backend/src/app/app.ts
+++ backend/src/app/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { Application } from "express-serve-static-core";
 import morgan from "morgan";
 
+import mainRouter from "../routes/main.js";
 import { timeNow } from "../helpers/utils.js";
 
 morgan.token("time-only", () => {
@@ -15,6 +16,8 @@ export function createApp(): Application {
     morgan("(:time-only) [MORGAN] :method :url :status - :response-time ms")
   );
   app.use(express.json());
+
+  app.use(mainRouter);
 
   return app;
 }
