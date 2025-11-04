1cf32be6236527ff5782b01014d3cfbe68e576c1
masiham <masihmooohmmadpoor@gmail.com>
2025-11-04 21:29:35 +0330

    created email verification

--- frontend/src/app/(auth)/signup/page.jsx
+++ frontend/src/app/(auth)/signup/page.jsx
@@ -6,6 +6,11 @@ import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { signup } from "@/services/authServices";
 import toast from "react-hot-toast";
+import { useState } from "react";
+import OtpInput from "react-otp-input";
+import { FaArrowLeftLong } from "react-icons/fa6";
+import { verifyEmail } from "@/services/authServices";
+import { useRouter } from "next/navigation";
 
 const schema = yup
   .object({
@@ -32,6 +37,10 @@ const schema = yup
   .required();
 
 const Signup = () => {
+  const router = useRouter();
+  const [step, setStep] = useState(1);
+  const [email, setEmail] = useState("");
+  const [otp, setOtp] = useState("");
   const {
     register,
     handleSubmit,
@@ -47,63 +56,122 @@ const Signup = () => {
     mode: "onTouched",
   });
 
-  const onSubmit = async (data) => {
+  const signupHandler = async (data) => {
     try {
       const { message } = await signup({
         name: data.name,
         email: data.email,
         password: data.password,
       });
+      setStep(2);
+      setEmail(data.email);
       toast.success(message);
     } catch (err) {
       toast.error(err?.response?.data?.message);
     }
   };
 
+  const emailVerificationHandler = async () => {
+    if (otp) {
+      try {
+        const { message } = await verifyEmail({ email, otp });
+        toast.success(message);
+        router.push("/");
+      } catch (err) {
+        toast.error(err?.response?.data?.message);
+      }
+    } else {
+      toast("لطفا کد تایید را وارد کنید", {
+        icon: "⚠️",
+      });
+    }
+  };
+
+  if (step === 1)
+    return (
+      <section className="flex justify-center pt-4">
+        <article className="w-[35%] my-5 rounded-lg bg-bg px-8 py-6 flex flex-col items-center">
+          <h1 className="text-4xl font-bold">فریشا</h1>
+          <h3 className="w-full mt-8 text-start text-xl font-semibold">
+            ثبت نام
+          </h3>
+          <form onSubmit={handleSubmit(signupHandler)} className="w-full mt-5">
+            <FormInput
+              register={register}
+              errors={errors}
+              label="نام کاربری"
+              name="name"
+            />
+            <FormInput
+              register={register}
+              errors={errors}
+              label="ایمیل"
+              name="email"
+            />
+            <FormInput
+              register={register}
+              errors={errors}
+              label="رمز عبور"
+              name="password"
+              type="password"
+            />
+            <FormInput
+              register={register}
+              errors={errors}
+              label="تکرار رمز عبور"
+              name="repeatPassword"
+              type="password"
+            />
+            <button
+              disabled={!isValid || !isDirty || isSubmitting}
+              className="w-full auth rounded-lg py-2 text-white mt-4 cursor-pointer disabled:cursor-not-allowed disabled:opacity-65"
+            >
+              ثبت نام
+            </button>
+          </form>
+          <Link href="/signin" className="text-sm text-secondary-700 mt-4">
+            قبلا ثبت نام کرده اید ؟
+            <span className="text-primary-500">ورود</span>
+          </Link>
+        </article>
+      </section>
+    );
   return (
     <section className="flex justify-center pt-4">
       <article className="w-[35%] my-5 rounded-lg bg-bg px-8 py-6 flex flex-col items-center">
         <h1 className="text-4xl font-bold">فریشا</h1>
-        <h3 className="w-full mt-8 text-start text-xl font-semibold">
-          ثبت نام
-        </h3>
-        <form onSubmit={handleSubmit(onSubmit)} className="w-full mt-5">
-          <FormInput
-            register={register}
-            errors={errors}
-            label="نام کاربری"
-            name="name"
-          />
-          <FormInput
-            register={register}
-            errors={errors}
-            label="ایمیل"
-            name="email"
-          />
-          <FormInput
-            register={register}
-            errors={errors}
-            label="رمز عبور"
-            name="password"
-            type="password"
-          />
-          <FormInput
-            register={register}
-            errors={errors}
-            label="تکرار رمز عبور"
-            name="repeatPassword"
-            type="password"
-          />
+        <div className="w-full mt-8 flex justify-between items-center">
+          <h3 className="text-xl font-semibold">کد تایید</h3>
           <button
-            disabled={!isValid || !isDirty || isSubmitting}
-            className="w-full auth rounded-lg py-2 text-white mt-4 cursor-pointer disabled:cursor-not-allowed disabled:opacity-65"
+            onClick={() => setStep(1)}
+            className="w-10 h-10 rounded-lg bg-primary-300 text-primary-800 cursor-pointer text-lg flex justify-center items-center"
           >
-            ثبت نام
+            <FaArrowLeftLong />
           </button>
-        </form>
-        <Link href="/signin" className="text-sm text-secondary-700 mt-4">
-          قبلا ثبت نام کرده اید ؟ <span className="text-primary-500">ورود</span>
-        </Link>
+        </div>
+        <div className="mt-5">
+          <p className="w-full text-center mb-5">لطفا کد تایید را وارد کنید</p>
+          <OtpInput
+            value={otp}
+            onChange={setOtp}
+            numInputs={5}
+            renderSeparator={<span className="text-primary-600">-</span>}
+            renderInput={(props) => (
+              <input {...props} style={{ width: "2.5rem" }} />
+            )}
+            inputStyle="inline-block border-2 border-primary-600 rounded-lg outline-none h-10 mx-4 text-center text-lg font-semibold"
+            containerStyle="flex flex-row-reverse"
+          />
+        </div>
+        <p className="w-full text-start my-5 text-sm font-semibold mr-2">
+          <span className="text-primary-600">90</span> ثانیه تا ارسال مجدد کد
+        </p>
+        <button
+          onClick={emailVerificationHandler}
+          className="w-full auth rounded-lg py-2 text-white cursor-pointer"
+        >
+          تایید
+        </button>
       </article>
     </section>
   );
