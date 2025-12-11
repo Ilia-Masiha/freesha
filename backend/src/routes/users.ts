eccd2916794eaf1bb2ec2add3deff5d5a8cb60c1
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-12-11 16:36:35 +0330

    GET users/:userId is fully implemented - Added this endpoint's route in routes/user.ts

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,11 +1,15 @@
 import express from "express";
 
-import { updateUser } from "../controllers/users.js";
-import { updateUserValidator } from "../middlewares/validation.js";
+import { getUser, updateUser } from "../controllers/users.js";
+import {
+  getUserValidator,
+  updateUserValidator,
+} from "../middlewares/validation.js";
 import { verifyUser } from "../middlewares/auth.js";
 
 const router = express.Router();
 
+router.get("/:userId", verifyUser, getUserValidator(), getUser);
 router.patch("/:userId", verifyUser, updateUserValidator(), updateUser);
 
 export default router;
