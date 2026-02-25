f2dc219f4093f9a843c7f2e3630a3c38d478e4f1
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2026-02-25 17:39:39 +0330

    Added limit and page query params

--- backend/src/database/sections/job_posts.ts
+++ backend/src/database/sections/job_posts.ts
@@ -16,6 +16,25 @@ import { DbResponse, DbResult, JobPost, None } from "../../helpers/types.js";
 import { jobPostsTable } from "../schema/job_posts.js";
 import { isNone } from "../../helpers/utils.js";
 
+const jobPostSelectFields = {
+  title: jobPostsTable.title,
+  slug: jobPostsTable.slug,
+  description: jobPostsTable.description,
+  budgetLow: jobPostsTable.budgetLow,
+  budgetHigh: jobPostsTable.budgetHigh,
+  deadline: jobPostsTable.deadline,
+
+  clientId: jobPostsTable.clientId,
+  statusId: jobPostsTable.statusId,
+  categoryId: jobPostsTable.categoryId,
+
+  requiredSkills: jobPostsTable.requiredSkills,
+  tags: jobPostsTable.tags,
+
+  createdAt: jobPostsTable.createdAt,
+  updatedAt: jobPostsTable.updatedAt,
+};
+
 export async function insertJobPost(
   jobPost: JobPost
 ): Promise<DbResponse<DbResult>> {
@@ -31,18 +50,17 @@ export async function insertJobPost(
   }
 }
 
-export async function getJobPost(
-  filters: Record<string, any>
+export async function getJobPosts(
+  filters: Record<string, any>,
+  limit: number,
+  page: number
 ): Promise<DbResponse<JobPost[] | None>> {
   const equalities = [];
+  const offset = (page - 1) * limit;
   let orderBy: SQL = asc(jobPostsTable.createdAt);
   let conditions;
 
   // Handling all of the filters
-  if (!isNone(filters.slug)) {
-    equalities.push(eq(jobPostsTable.slug, filters.slug));
-  }
-
   if (!isNone(filters.clientId)) {
     equalities.push(eq(jobPostsTable.clientId, filters.clientId));
   }
@@ -80,28 +98,28 @@ export async function getJobPost(
 
   try {
     const result = await db
-      .select({
-        title: jobPostsTable.title,
-        slug: jobPostsTable.slug,
-        description: jobPostsTable.description,
-        budgetLow: jobPostsTable.budgetLow,
-        budgetHigh: jobPostsTable.budgetHigh,
-        deadline: jobPostsTable.deadline,
-
-        clientId: jobPostsTable.clientId,
-        statusId: jobPostsTable.statusId,
-        categoryId: jobPostsTable.categoryId,
-
-        requiredSkills: jobPostsTable.requiredSkills,
-        tags: jobPostsTable.tags,
-
-        createdAt: jobPostsTable.createdAt,
-        updatedAt: jobPostsTable.updatedAt,
-      })
+      .select(jobPostSelectFields)
       .from(jobPostsTable)
       .where(conditions)
+      .limit(limit)
+      .offset(offset)
       .orderBy(orderBy);
     return makeDbResponse<JobPost[] | None>(result, null);
+  } catch (error) {
+    return makeDbResponse(null, error as Error);
+  }
+}
+
+export async function getJobPost(
+  slug: string
+): Promise<DbResponse<JobPost | None>> {
+  try {
+    const result = await db
+      .select(jobPostSelectFields)
+      .from(jobPostsTable)
+      .where(eq(jobPostsTable.slug, slug));
+
+    return makeDbResponse<JobPost | None>(result[0], null);
   } catch (error) {
     return makeDbResponse(null, error as Error);
   }
